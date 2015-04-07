5c425dc2c209ec4d173167497775d92c3f1a9c46
Wouter Timmermans <wouter@woutertimmermans.nl>
2015-04-07 14:15:25 +0200

    Versturen zou moeten werken

--- src/main/java/SuperTransport.java
+++ src/main/java/SuperTransport.java
@@ -1,6 +1,8 @@
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
@@ -14,15 +16,18 @@ public class SuperTransport implements Transport {
 
     private Network networkLayer;
 
-    private Queue<Byte[]> unAckedData;
+    private Queue<byte[]> unAckedData;
 
     private Map<InetAddress, Queue<Byte>> sendQueues;
+
+    private boolean running;
 
     // ------------------- Constructors ---------------------
 
     public SuperTransport(Network networkLayer) {
 
-        this.networkLayer = networkLayer;
+        this.networkLayer = null;
+        running = true;
 
     }
 
@@ -36,25 +41,52 @@ public class SuperTransport implements Transport {
     @Override
     public void send(InetAddress dest, byte[] data) {
 
+        Queue<Byte> queue = sendQueues.get(dest);
 
-        TransportSegment newPacket = new TransportSegment(data);
-        //sendQueues.get(dest).add(newPacket.toByteArray());
+        for(byte b : data) {
+            queue.add(b);
+        }
 
-        networkLayer.send(dest, data);
+        processSendQueue();
 
     }
 
     @Override
     public void processPacket(DatagramPacket packet) {
 
+        InetAddress source = packet.getAddress();
+        byte[] data = packet.getData();
+
+
+
+
     }
+
+    public void processSendQueue() {
+
+        for(Map.Entry<InetAddress, Queue<Byte>> entry : sendQueues.entrySet()) {
+
+            List<Byte> data = new LinkedList<>();
+
+            while(entry.getValue().poll() != null && data.size() < 1400) {
+                data.add(entry.getValue().remove());
+            }
+
+            networkLayer.send(entry.getKey(), new TransportSegment(data.toArray(new Byte[data.size()])).toByteArray());
+
+
+        }
+
+    }
+
+
 
     class TransportSegment {
 
         long timeStamp;
-        byte[] data;
+        Byte[] data;
 
-        TransportSegment(byte[] data) {
+        TransportSegment(Byte[] data) {
 
             timeStamp = System.currentTimeMillis();
             this.data = data;
@@ -62,9 +94,16 @@ public class SuperTransport implements Transport {
         }
 
         byte[] toByteArray() {
+            byte[] primBytes = new byte[data.length];
+            int i = 0;
+            for (Byte b : data) {
+                primBytes[i] = b;
+            }
+
             ByteBuffer buf = ByteBuffer.allocate(data.length + Long.BYTES);
-            buf.put(data);
+            buf.put(primBytes);
             buf.putLong(timeStamp);
+
             return buf.array();
 
         }
