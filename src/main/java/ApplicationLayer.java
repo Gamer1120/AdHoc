237b05b7f799204dbd92063be4324a2dd0e99e78
Gamer1120 <Gamer1120@github.com>
2015-04-07 12:38:26 +0200

    ApplicationLayer met support voor Files.

--- src/main/java/ApplicationLayer.java
+++ src/main/java/ApplicationLayer.java
@@ -1,17 +1,37 @@
+/**
+ * Application Layer for the Ad hoc multi-client chat application.
+ * 
+ * @author Michael Koopman s1401335, Sven Konings s1534130, Wouter ??? s???, René Boschma s???
+ */
+import java.io.File;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class ApplicationLayer implements Application {
 
+	/**
+	 * Sends a packet to the Network Layer.
+	 */
 	@Override
 	public void send(InetAddress dest, Object input) {
 		if (input instanceof String) {
+			// TODO: Add some bytes to determine whether the message is text or
+			// a file.
 			NetworkLayer.send(dest, ((String) input).getBytes());
+		} else if (input instanceof File) {
+			Path path = Paths.get(((File) input).getAbsolutePath());
+			NetworkLayer.sendFile(dest, Files.readAllBytes(path));
 		}
 	}
 
+	/**
+	 * Sends a packet to the GUI.
+	 */
 	@Override
 	public void processPacket(DatagramPacket packet) {
 		byte[] bytestream = packet.getData();
