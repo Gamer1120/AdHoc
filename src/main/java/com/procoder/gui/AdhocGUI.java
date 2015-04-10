95d6563657a1a52d1ea6027dbeb007feb4b89759
boschma2702 <reneboschma@hotmail.com>
2015-04-10 11:57:37 +0200

    source and des added

--- src/main/java/com/procoder/gui/AdhocGUI.java
+++ src/main/java/com/procoder/gui/AdhocGUI.java
@@ -7,12 +7,12 @@ import javafx.scene.image.Image;
 @SuppressWarnings("restriction")
 public interface AdhocGUI {
 
-    public void addMsg(String msg);
+    //public void addMsg(String msg);
 
-    public void processString(String user, String msg);
+    public void processString(String source, String destination, String msg);
 
-    public void processFile(String user, File file);
+    public void processFile(String source, String destination, File file);
 
-    public void processImage(String user, Image img);
+    public void processImage(String source, String destination, Image img);
 
 }
