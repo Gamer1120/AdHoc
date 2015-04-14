5b971e3c8388dd2ac6e570748178f5f43caa0936
Wouter Timmermans <wouter@woutertimmermans.nl>
2015-04-14 11:22:10 +0200

    Possible fix voor adhoc

--- src/main/java/com/procoder/transport/TransportConnection.java
+++ src/main/java/com/procoder/transport/TransportConnection.java
@@ -242,7 +242,7 @@ public class TransportConnection {
             // ACK op een syn ack
             established = true;
             LOGGER.debug("[TL] [RCV] Verbinding tussen {} en {} is nu in de state established", NetworkUtils.getLocalHost().getHostAddress(), receivingHost);
-        } else if (!established) {
+        } else if (!established && !synSent && !synReceived) {
             // Andere gevallen stuur een reset.
             LOGGER.debug("Ik stuur nu een RESET");
             TransportSegment syn = new TransportSegment(new Byte[0], seq);
