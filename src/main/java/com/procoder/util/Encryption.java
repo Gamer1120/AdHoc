98de01dc37616a7e06f08245c09973c53b0da0f6
boschma2702 <reneboschma@hotmail.com>
2015-04-14 14:50:39 +0200

    SECRET KEY added
    niet doorvertellen he

--- src/main/java/com/procoder/util/Encryption.java
+++ src/main/java/com/procoder/util/Encryption.java
@@ -6,10 +6,41 @@ package com.procoder.util;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Encryption {
+
+    public static final byte[] KEY = new byte[]{43, 93, 99, -108, 2, 7, 32, 16, -72, 121, 68, 127, 99, 125, -88, 21};
+
+
+    /*
+    public Encryption(){
+        /*
+        byte[] encrypted = Encryption.getEncrypted("Dit is een test".getBytes(), new SecretKeySpec(KEY, 0, KEY.length, "AES"));
+        byte[] decrypted = Encryption.getDecrypted(encrypted, new SecretKeySpec(KEY, 0, KEY.length, "AES"));
+        System.out.println(new String(decrypted));
+
+
+        /*
+        SecretKey sk = Encryption.generateKey();
+        byte[] data = sk.getEncoded();
+        for(int i=0; i<data.length;i++){
+            System.out.print(data[i]+" ");
+        }
+
+        //System.out.println(data.length);
+    }
+
+    public static void main(String[] args){
+        new Encryption();
+
+    }*/
+
+
 
 
     public static byte[] getEncrypted(byte[] array, SecretKey key){
